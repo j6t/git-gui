d2d3efaf33bb0a5527dd11086ac7f231d62fb102
Johannes Sixt <j6t@kdbg.org>
2025-07-20 12:47:40 +0200

    fixup! Merge branch 'js/ask-yesno' into j6t-testing
    Signed-off-by: Johannes Sixt <j6t@kdbg.org>

--- git-gui.sh
+++ git-gui.sh
@@ -1131,10 +1131,10 @@ if {![info exists env(SSH_ASKPASS)]} {
 	set env(SSH_ASKPASS) [file join [git --exec-path] git-gui--askpass]
 }
 if {![info exists env(GIT_ASKPASS)]} {
-	set env(GIT_ASKPASS) [gitexec git-gui--askpass]
+	set env(GIT_ASKPASS) [file join [git --exec-path] git-gui--askpass]
 }
 if {![info exists env(GIT_ASK_YESNO)]} {
-	set env(GIT_ASK_YESNO) [gitexec git-gui--askyesno]
+	set env(GIT_ASK_YESNO) [file join [git --exec-path] git-gui--askyesno]
 }
 
 ######################################################################
